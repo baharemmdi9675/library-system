a6cfae62f9680556041d2a382276118d5f74d047
Mohammad Sharif <msharifdev@gmail.com>
2026-09-17 16:10:15 +0330

    feat: Random Generate

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
+import java.util.Random;
+
 public class Main {
     static void main(String[] args) {
         System.out.println("test");
+
+        Random random = new Random();
+        int randNum = random.nextInt();
+        System.out.println(randNum);
+
     }
 }
