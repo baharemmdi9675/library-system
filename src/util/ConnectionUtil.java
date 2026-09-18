99739ce27e8d00ce39deabf24ead1ceefeb7d124
Mohammad Sharif <msharifdev@gmail.com>
2026-09-18 15:22:03 +0330

    add databasae connection

---
+++ src/util/ConnectionUtil.java
@@ -0,0 +1,17 @@
+package util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionUtil {
+    private static final String URL = "jdbc:postgresql://localhost:5432/database_library";
+    private static final String USER = "postgres";
+    private static final String PASSWORD = "123456789";
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+
+
+}
